e418cedf30df3d399a91d27cdce5321cf9ec7800
JAVIERTEL <javierlaguna2001@gmail.com>
2024-03-29 00:58:23 +0100

    añadir HomePage a la App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Cabecera from './Cabecera'; // Importa el componente Cabecera
+import HomePage from './HomePage'; // Importa el componente HomePage
 
 function App() {
   return (
     <div className="App">
       <Cabecera />
+      <HomePage/>
     </div>
   );
 }
