a07bc4d4887de25ca31e4596befdeb4e14d03e65
JAVIERTEL <javierlaguna2001@gmail.com>
2024-03-29 00:59:42 +0100

    resolver conflictos de bootstrap

--- src/Cabecera.js
+++ src/Cabecera.js
@@ -4,22 +4,22 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 function Cabecera() {
   return (
     <div id="cabecera">
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="transparent" expand="lg">
         <Navbar.Collapse id="basic-navbar-nav">
           <Container>
             <Nav className="navbar-links ms-auto">
-              <Nav.Link href="#home">HomePage</Nav.Link>
-              <Nav.Link href="#places">Places</Nav.Link>
-              <Nav.Link href="#map">Map</Nav.Link>
+              <Nav.Link href="HomePage">HomePage</Nav.Link>
+              <Nav.Link href="Places">Places</Nav.Link>
+              <Nav.Link href="Map">Map</Nav.Link>
             </Nav>
           </Container>
         
         </Navbar.Collapse>
       </Navbar>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="transparent" expand="lg">
         <Navbar.Collapse id="basic-navbar-nav">
           <Container id="cab-right">
-          <img className="logo" src={process.env.PUBLIC_URL + "/logomascotmercio.png"} alt="logo" />
+          <img className="logo-cabecera" src={process.env.PUBLIC_URL + "/logomascotmercio.png"} alt="logo" />
             <Nav className="navbar-links ms-auto">
               <Nav.Link href="#Login">Log in</Nav.Link>
             </Nav>
