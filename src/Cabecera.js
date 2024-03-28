71ff79c1542ff8d918a43fcaf1f8106c6a5700f6
JAVIERTEL <javierlaguna2001@gmail.com>
2024-03-28 21:13:42 +0100

    Añadir logo, letras cabecera en blanco con fondo negro, más quitar código que ya venía hecho que no servía

--- src/Cabecera.js
+++ src/Cabecera.js
@@ -2,23 +2,24 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function Cabecera() {
-  return (
+  return (<div id="cabecera">
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+      <img className = "logo" src= {process.env.PUBLIC_URL + "/logomascotmercio.png"} alt="logo"/>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">HomePage</Nav.Link>
-            <Nav.Link href="#places">Places</Nav.Link>
-            <Nav.Link href="#map">Map</Nav.Link>
+            <Nav.Link href="#home" className="nav-link-left">HomePage</Nav.Link>
+            <Nav.Link href="#places"  className="nav-link-left">Places</Nav.Link>
+            <Nav.Link href="#map"  className="nav-link-left">Map</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#login">Log In</Nav.Link>
+            <Nav.Link href="#login"  className="nav-link-right">Log In</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
   );
 }
 
