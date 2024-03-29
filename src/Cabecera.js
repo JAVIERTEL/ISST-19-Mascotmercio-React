e21f2409c7006ddc2a088f6a6e916f61cebff439
JAVIERTEL <javierlaguna2001@gmail.com>
2024-03-29 09:20:22 +0100

    conflicto de bootstrap

--- src/Cabecera.js
+++ src/Cabecera.js
@@ -20,7 +20,7 @@ function Cabecera() {
           <Container id="cab-right">
           <img className="logo-cabecera" src={process.env.PUBLIC_URL + "/logomascotmercio.png"} alt="logo" />
             <Nav className="navbar-links ms-auto">
-              <Nav.Link href="#Login">Log in</Nav.Link>
+              <Nav.Link href="Login" style={{ color: 'white' }}>Log in</Nav.Link>
             </Nav>
           </Container>
         </Navbar.Collapse>
