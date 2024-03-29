0be31b601e2da4e08972534d43d25140cd3e2733
JAVIERTEL <javierlaguna2001@gmail.com>
2024-03-29 10:38:10 +0100

    Comienzo desarrollo vista Places

--- src/Componentes/Places.js
+++ src/Componentes/Places.js
@@ -1,7 +1,38 @@
+import React, { useState} from 'react';
+import  Button  from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 function Places() {
+
+    const [tienda,setTienda] = useState ([]);
+
     return (
 <div>
+<h2 className='text-center'>Places</h2>
+<table className='table table-bordered table-striped'>
+        <thead>
+            <th>Place Name</th>
+            <th>Dirección</th>
+            <th>Servicios</th>
+            <th>Actions</th>
+        </thead>
+        <tbody>
+            {
+                tienda.map (
+                    tienda =>
+                    <tr key= {tienda.id_tienda}>
+                        <td>{tienda.nombre}</td>
+                        <td>{tienda.dirección}</td>
+                        <td>{tienda.servicios}</td>
+                        <td>
+                        <Button variant="blue" >Update</Button>
+                        <Button variant="red" >Delete</Button>
+                        </td>
+                    </tr>
+                )
+            }
+        </tbody>
 
+</table>
 <span>Lo que habrá que hacer aquí es crear un fichero en servicios que haga
 una petición a la api y que saque todos los locales y a continuación implemetar
 esa vista aquí junto con los botones Update y Delete
