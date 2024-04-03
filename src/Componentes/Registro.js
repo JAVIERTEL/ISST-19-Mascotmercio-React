a81fc8a6a14e18296d1bac6f677fcb8d0aae9e08
mlumbrerasl02 <mlumbrerasl02@gmail.com>
2024-04-03 19:52:17 +0200

    Pagina de registro crea un cliente

--- src/Componentes/Registro.js
+++ src/Componentes/Registro.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import apiServiceInstance from '../services/ApiService';
+
 
 function Registro() {
+
+  const [email, setEmail] = useState('');
+  const [usuario, setUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  //const [contrasena2, setContrasena2] = useState('');
+
+
+  const handleCrearCliente = async(usuario, contrasena, email)  => {
+    try {
+      
+      await apiServiceInstance.crearCliente(usuario, contrasena, email);
+      
+  } catch (error) {
+      console.error('Error creando cliente:', error);
+  }
+  }
+
   return (
     <div className="register-container" style={{ maxWidth: '500px', margin: '0 auto' }}>
       <h2 style={{ textAlign: 'center' }}>Registro</h2>
       <div className="form-group mb-3">
         <label htmlFor="email" className="form-label">Dirección de correo electrónico:</label>
-        <input type="text" id="email" className="form-control" />
+        <input type="text" id="email" className="form-control"  value={email} onChange={(e) => setEmail(e.target.value)}/>
       </div>
       <div className="form-group mb-3">
         <label htmlFor="username" className="form-label">Nombre de usuario:</label>
-        <input type="text" id="username" className="form-control" />
+        <input type="text" id="username" className="form-control" value={usuario} onChange={(e) => setUsuario(e.target.value)}/>
       </div>
       <div className="form-group mb-3">
         <label htmlFor="password" className="form-label">Contraseña:</label>
-        <input type="password" id="password" className="form-control" />
+        <input type="password" id="password" className="form-control" value={contrasena} onChange={(e) => setContrasena(e.target.value)}  />
       </div>
       <div className="form-group mb-3">
         <label htmlFor="password2" className="form-label">Repetir Contraseña:</label>
         <input type="password" id="password2" className="form-control" />
       </div>
       <div className="d-grid mb-3">
-        <Button variant="success" type="submit">Regístrate</Button>
+        <Button variant="success" type="submit" onClick= {() => handleCrearCliente(usuario, contrasena, email)}>Regístrate</Button>
       </div>
       <p className="mb-0 text-center">¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link></p>
     </div>
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,6 +1,67 @@
 import axios from 'axios';
 
+const Cliente_URL = "http://localhost:8085/api/cliente";
+const Propietario_URL = "http://localhost:8085/api/propietario";
+const Resenas_URL = "http://localhost:8085/api/resenas";
+
+
 class ApiService{
+
+    //Cliente
+    getClientes = async () => {
+        try {
+            const response = await axios.get(Cliente_URL + '/findAll');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching clientes', error);
+            throw error;
+        }
+    }
+
+    crearCliente = async (usuario, contrasena, email) => {
+        try{
+            const nuevoCliente = {
+                "usuario" : usuario,
+                "contraseña": contrasena,
+                "email" : email
+            }
+
+            const response = await axios.post(Cliente_URL, nuevoCliente);
+            return response.data;
+        } catch (error) {
+            console.error('Error creando cliente', error);
+            throw error;
+        }
+    }
+
+
+
+    //Propietario
+
+    getPropietarios = async () => {
+        try {
+            const response = await axios.get(Propietario_URL + '/findAll');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching propietario', error);
+            throw error;
+        }
+    }
+
+
+    // Reseñas 
+
+    getResenas = async () => {
+        try {
+            const response = await axios.get(Resenas_URL + '/findAll');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching reseñas', error);
+            throw error;
+        }
+    }
+
+
     
 }
 
