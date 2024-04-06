cdf23755735781dc79b253806585e35df388c871
Ignacio Diez <ignacio.divergara@alumnos.upm.es>
2024-04-06 12:16:34 +0200

    mapaconectado

--- src/Componentes/Map.js
+++ src/Componentes/Map.js
@@ -1,65 +1,73 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import apiServiceInstance from '../services/ApiService';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function MapboxMap() {
-    useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamFuZHJvbWRlbGFtb3JlbmEiLCJhIjoiY2x1ZWRydmxiMTdmdDJqbnNuZ2dmOG13byJ9.hgXHzxrICsmRH4kPljAEvw';
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [-3.727238, 40.444001 ],
-            zoom: 13
-        });
+    const [tiendas, setTiendas] = useState([]);
 
-        const geojson = {
-            'type': 'FeatureCollection',
-                    'features': [
-                        {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const tiendasData = await apiServiceInstance.getTiendas();
+                setTiendas(tiendasData);
+
+                // Crear el mapa solo si hay datos de tiendas disponibles
+                if (tiendasData.length > 0) {
+                    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamFuZHJvbWRlbGFtb3JlbmEiLCJhIjoiY2x1ZWRydmxiMTdmdDJqbnNuZ2dmOG13byJ9.hgXHzxrICsmRH4kPljAEvw';
+                    const map = new mapboxgl.Map({
+                        container: 'map',
+                        style: 'mapbox://styles/mapbox/streets-v12',
+                        center: [-3.727238, 40.444001],
+                        zoom: 13
+                    });
+
+                    const geojson = {
+                        'type': 'FeatureCollection',
+                        'features': tiendasData.map(tienda => ({
                             'type': 'Feature',
                             'properties': {
-                                'title': '<strong>Make it Mount Pleasant</strong>',
-                                'description':
-                                    '<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>'
+                                'title': `<strong>${tienda.nombre}</strong>`,
+                                'description': `<p>${tienda.description}</p>`
                             },
                             'geometry': {
                                 'type': 'Point',
-                                'coordinates': [-3.727238, 40.444001 ]
+                                'coordinates': [-3.727238, 40.444001]
                             }
-                        },
-                        // Otras características...
-                    ]
-                };
+                        }))
+                    };
 
-                // add markers to map
-for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-  
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el)
-    .setLngLat(feature.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML(
-            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
-          )
-      )
-    .addTo(map);
-  }
-    },[]);
-        
-        
-        
-           
-           
+                    // Agregar marcadores al mapa
+                    for (const feature of geojson.features) {
+                        const el = document.createElement('div');
+                        el.className = 'marker';
 
-      
-    
+                        new mapboxgl.Marker(el)
+                            .setLngLat(feature.geometry.coordinates)
+                            .setPopup(
+                                new mapboxgl.Popup({ offset: 25 })
+                                    .setHTML(
+                                        `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+                                    )
+                            )
+                            .addTo(map);
+                    }
+
+                    // Limpia el mapa cuando el componente se desmonta
+                    return () => map.remove();
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h1>Mapa en desarrollo por Alex</h1>
-        <div id="map" style={{ position: 'fixed', top: 150, bottom: 110, width: '80%' }}></div>
+            <div id="map" style={{ position: 'fixed', top: 150, bottom: 110, width: '80%' }}></div>
         </div>
     );
 }
