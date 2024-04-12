2f27757d46b021180f736741349c643d5818fdad
ignacioo-diez <ignacio.divergara@alumnos.upm.es>
2024-04-12 11:56:14 +0200

    filtrado

--- src/Componentes/Map.js
+++ src/Componentes/Map.js
@@ -9,6 +9,8 @@ function MapboxMap() {
     const [menuAbierto, setMenuAbierto] = useState(false);
     const [tiendasConCoordenadas, setTiendasConCoordenadas] = useState([]);
     const [servicios, setServicios] = useState([]);
+    const [serviciosSeleccionados, setServiciosSeleccionados] = useState([]);
+
 
     const geocodeAddress = async (direccion) => {
         const addressToGeocode = `${direccion}.json`; // Corregido: Use de comillas invertidas (`) en lugar de comillas simples (')
@@ -37,11 +39,42 @@ function MapboxMap() {
         }
     };
 
+    const obtenerNombresBooleanos = () => {
+        const nombres = [];
+        for (const servicio of servicios) {
+            for (const clave in servicio) {
+                if (typeof servicio[clave] === 'boolean' && !nombres.includes(clave)) {
+                    
+                    nombres.push(clave);
+                }
+            }
+        }
+        return nombres;
+    };
+
+    const handleCheckboxChange = (event) => {
+        const servicioSeleccionado = event.target.value;
+        if (event.target.checked) {
+            // Si la casilla está marcada, agregamos el servicio seleccionado al array
+            setServiciosSeleccionados([...serviciosSeleccionados, servicioSeleccionado]);
+            console.log(serviciosSeleccionados)
+
+        } else {
+            // Si la casilla está desmarcada, eliminamos el servicio seleccionado del array
+            setServiciosSeleccionados(serviciosSeleccionados.filter(servicio => servicio !== servicioSeleccionado));
+            console.log(serviciosSeleccionados)
+        }
+    };
+
+    // Func
+
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const tiendasData = await apiServiceInstance.getTiendas();
                 const serviciosData = await apiServiceInstance.getServicios();
+                
                 setServicios(serviciosData);
                 setTiendas(tiendasData);
             } catch (error) {
@@ -52,7 +85,7 @@ function MapboxMap() {
     }, []);
 
     useEffect(() => {
-        if (tiendas.length > 0) {
+        if (servicios.length > 0) {
             const geocodeTiendas = async () => {
                 const tiendasConCoordenadas = await Promise.all(servicios.map(async (servicio) => {
                     const coordenadas = await geocodeAddress(servicio.tienda.direccion);
@@ -65,21 +98,29 @@ function MapboxMap() {
 
             geocodeTiendas();
         }
-    }, [tiendas]);
+    }, [tiendas,servicios]);
 
     useEffect(() => {
         if (tiendasConCoordenadas.length > 0) {
+
             mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamFuZHJvbWRlbGFtb3JlbmEiLCJhIjoiY2x1ZWRydmxiMTdmdDJqbnNuZ2dmOG13byJ9.hgXHzxrICsmRH4kPljAEvw';
             const map = new mapboxgl.Map({
                 container: 'map',
                 style: 'mapbox://styles/mapbox/streets-v12',
                 center: [-3.727238, 40.444001],
-                zoom: 9
+                zoom: 10
             });
+
+            const tiendasFiltradas = tiendasConCoordenadas.filter(tienda => {
+                return serviciosSeleccionados.every(servicio => tienda[servicio]);
+            });
+
+            
+            
 
             const geojson = {
                 'type': 'FeatureCollection',
-                'features': tiendasConCoordenadas.map((servicio, index) => ({
+                'features': tiendasFiltradas.map((servicio, index) => ({
                     'type': 'Feature',
                     'properties': {
             'title': `<strong>${servicio.tienda.nombre}</strong>`,
@@ -115,28 +156,62 @@ function MapboxMap() {
 
             return () => map.remove();
         }
-    }, [tiendasConCoordenadas]);
+    }, [tiendasConCoordenadas,serviciosSeleccionados]);
 
     const toggleMenu = () => {
         setMenuAbierto(!menuAbierto);
     };
 
+
+    const nombresBooleanos = obtenerNombresBooleanos();
+
+            let tiendasFiltradas = tiendasConCoordenadas;
+
+            if (serviciosSeleccionados.length>0) {
+                tiendasFiltradas = tiendasConCoordenadas.filter(tienda => {
+                    return serviciosSeleccionados.every(servicio => tienda[servicio]);
+                });
+            }
+
     const renderMenu = () => {
         if (menuAbierto) {
+            
+
+
+
+            
+        
+
             return (
                 <div className="menu">
+
+              
+  
+
+
                     <h2>Tiendas cercanas</h2>
+
+
                     <ul>
-                        {servicios.map(servicio => (
+                        {tiendasFiltradas.map(servicio => (
+
+                            
+
+                            
                             <li key={servicio.tienda.idTienda}>
+                                
                                 <strong>{servicio.tienda.nombre}</strong>
                                 <p>{servicio.tienda.direccion}</p>
                                 <ul className='servicios-list'>
-            {Object.entries(servicio).map(([clave, valor]) => (
-                (clave !== 'nombre' && clave !== 'direccion' && typeof valor === 'boolean' && valor) && 
-                <li key={clave} >{clave}</li>
-            ))}
-        </ul>
+                                {Object.entries(servicio).map(([clave, valor]) => (
+               
+                                (clave !== 'nombre' && clave !== 'direccion' && typeof valor === 'boolean' && valor) && 
+                                <li key={clave}>
+                                 {clave.replace(/_/g, ' ').replace(/\b\w/g, c => c.toUpperCase())} {/* Capitaliza la primera letra de cada palabra */}
+                                 </li>
+                                ))}
+                                   
+                                </ul>
                             </li>
                         ))}
                     </ul>
@@ -146,8 +221,34 @@ function MapboxMap() {
         return null;
     };
 
+
+
     return (
         <div>
+              <div className='filtro'>
+                <h3>Selecciona tus opciones:</h3>
+                <ul>
+                    {nombresBooleanos.map( nombre => (
+                        <li key={nombre}>
+                            <input
+                            type='checkbox'
+                            id={nombre}
+                            value={nombre}
+                            checked={serviciosSeleccionados.includes(nombre)}
+                            onChange={handleCheckboxChange}
+                            />
+                            <label htmlFor={nombre}>{nombre}</label>
+
+                        </li>
+                    ))}
+
+                </ul>
+               
+                 
+             
+            </div>
+
+
             <div id="map-container" className={menuAbierto ? 'menu-abierto' : ''}>
                 {renderMenu()}
                 <div id="map"></div>
